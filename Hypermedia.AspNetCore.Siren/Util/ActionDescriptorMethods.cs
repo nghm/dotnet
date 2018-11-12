2dc13ca210198ae22f4658fa397615df10b768ab
Dan Patiu <dan.patiu@pitechnologies.ro>
2018-11-12 16:15:24 +0200

    ref: removing unused code

--- Hypermedia.AspNetCore.Siren/Util/ActionDescriptorMethods.cs
+++ Hypermedia.AspNetCore.Siren/Util/ActionDescriptorMethods.cs
@@ -33,40 +33,7 @@
                 .PickBodyArguments(arguments)
                 .SingleOrDefault();
         }
-
-        public static IEnumerable<object> PickQueryArguments(this ActionDescriptor descriptor, object[] arguments)
-        {
-            return descriptor.PickArguments(arguments, "Query");
-        }
-
-        public static object BuildQueryObject(this ActionDescriptor descriptor, object[] arguments)
-        {
-            return BuildObject(descriptor, arguments, "Query");
-        }
-
-        public static object BuildRouteObject(this ActionDescriptor descriptor, object[] arguments)
-        {
-            return BuildObject(descriptor, arguments, "Path");
-        }
-
-        private static IEnumerable<KeyValuePair<string, object>> BuildObject(this ActionDescriptor descriptor, object[] arguments, string id)
-        {
-            var parameters = descriptor.Parameters;
-            var index = -1;
-
-            foreach (var parameter in parameters)
-            {
-                index++;
-
-                if (parameter.BindingInfo.BindingSource.Id != id)
-                {
-                    continue;
-                }
-
-                yield return KeyValuePair.Create(parameter.Name, arguments[index]);
-            }
-        }
-
+        
         private static IEnumerable<object> PickArguments(this ActionDescriptor descriptor, object[] arguments, string id)
         {
             var parameters = descriptor.Parameters;
