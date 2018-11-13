567da894dfae04249a2dbd4eb1def885c374956b
Dan Patiu <dan.patiu@pitechnologies.ro>
2018-11-13 16:15:44 +0200

    feat(external-api): added assembly loader implementation

---
+++ Hypermedia.AspNetCore.ApiExport.Tests/AssemblyLoaderTests.cs
@@ -0,0 +1,25 @@
+﻿namespace Hypermedia.AspNetCore.ApiExport.Tests
+{
+    using System;
+    using System.IO;
+    using AssemblyAnalyzer;
+    using Objectivity.AutoFixture.XUnit2.AutoMoq.Attributes;
+    using Xunit;
+
+    public class AssemblyLoaderTests
+    {
+        [Theory]
+        [AutoMockData]
+        void LoadThrowsArgumentException(string path, AssemblyLoader assemblyLoader)
+        {
+            Assert.Throws<ArgumentException>(() => assemblyLoader.Load(path));
+        }
+
+        [Theory]
+        [AutoMockData]
+        void LoadThrowsFileNotFoundException(string path, AssemblyLoader assemblyLoader)
+        {
+            Assert.Throws<FileNotFoundException>(() => assemblyLoader.Load($"c:\\{path}"));
+        }
+    }
+}
---
+++ Hypermedia.AspNetCore.ApiExport/AssemblyAnalyzer/AssemblyLoader.cs
@@ -0,0 +1,12 @@
+﻿namespace Hypermedia.AspNetCore.ApiExport.AssemblyAnalyzer
+{
+    using System.Reflection;
+
+    internal class AssemblyLoader : IAssemblyLoader
+    {
+        public Assembly Load(string path)
+        {
+            return Assembly.LoadFile(path);
+        }
+    }
+}
